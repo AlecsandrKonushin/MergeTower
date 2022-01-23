67cb13f0a881c48ddad38d67e80011fe17b53148
DESKTOP-PIS374D\79537 <aleksander.conushin@yandex.ru>
2022-01-23 15:09:49 +0300

    Buy tower after click button

---
+++ MergeTower/Assets/Scripts/Controllers/ControllerSpawnTower.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "ControllerEffects", menuName = "Controllers/ControllerSpawnTower")]
+public class ControllerSpawnTower : ControllerBase, IAwake, IListenerBuyTower
+{
+    public void OnAwake()
+    {
+        ActionsGame.Instance.AddListenerBuyTower(this);
+    }
+
+    public void BuyTower()
+    {
+        Debug.Log("устанавливаем");
+    }
+
+}
---
+++ MergeTower/Assets/Scripts/Objects/TilesParent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TilesParent : Singleton<TilesParent>
+{
+    private Tile[] tiles;
+
+    private List<Tile> freeTiles = new List<Tile>();
+
+    protected override void AfterAwaik()
+    {
+        tiles = GetComponentsInChildren<Tile>();
+        freeTiles.AddRange(tiles);
+    }
+
+    public Tile GetTileForSpawnTower()
+    {
+        Tile tile = null;
+
+        if (freeTiles.Count == 0)
+        {
+            Debug.Log("Нет свободных tiles для спауна tower");
+
+            return null;
+        }
+
+        if (freeTiles.Count == 1)
+        {
+            tile = freeTiles[0];
+        }
+
+        tile = freeTiles[Random.Range(0, freeTiles.Count)];
+
+        freeTiles.Remove(tile);
+
+        return tile;
+    }
+}
