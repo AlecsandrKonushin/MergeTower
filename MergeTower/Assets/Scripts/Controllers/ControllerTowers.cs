836204a062f070bb995586dbdfee528ff5819c82
DESKTOP-PIS374D\79537 <aleksander.conushin@yandex.ru>
2022-01-23 15:34:09 +0300

    Spawn tower

---
+++ MergeTower/Assets/Scripts/Controllers/ControllerTowers.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ControllerEffects", menuName = "Controllers/ControllerTowers")]
+public class ControllerTowers : ControllerBase, IAwake, IListenerBuyTower
+{
+    [SerializeField] private Tower[] towerPrefabs;
+
+    private List<Tower> towers= new List<Tower>();
+
+    public void OnAwake()
+    {
+        ActionsGame.Instance.AddListenerBuyTower(this);
+    }
+
+    public void BuyTower()
+    {
+        Tile tile = TilesParent.Instance.GetTileForSpawnTower();
+
+        if(tile == null)
+        {
+            return;
+        }
+        else
+        {
+            Tower newTower = CreatorObjects.Instance.CreateTower(towerPrefabs[0], tile);
+
+            towers.Add(newTower);
+        }
+    }
+
+}
---
+++ MergeTower/Assets/Scripts/Main/CreatorObjects.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CreatorObjects : Singleton<CreatorObjects>
+{
+    [SerializeField] private GameObject parentTowers;
+
+    public Tower CreateTower(Tower towerPrefab, Tile tileForSpawn)
+    {
+        Vector3 positionSpawn = tileForSpawn.transform.position;
+        positionSpawn.y += 1f;
+
+        Tower tower = Instantiate(towerPrefab, positionSpawn, tileForSpawn.transform.rotation);
+        tower.transform.SetParent(parentTowers.transform);
+
+        return tower;
+    }
+}
