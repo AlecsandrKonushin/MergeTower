3f3c4b41b19d76fdc4a2f0943349eaf017c2ce59
DESKTOP-DJDFSPP\artem <artem12_98@mail.ru>
2022-02-11 01:04:10 +0700

    сделал панели рестарт, покупка башни \ добавил на всякий шейдера воды

---
+++ MergeTower/Assets/Scripts/UI/RestartPanel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RestartPanel : Panel
+{
+    public Button RestarButton;
+    public Button MenuExit;
+}
