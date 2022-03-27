1854227b9d14cd033a7b6377a41840166e0526ee
DESKTOP-7MM2106\unity <aleksander.conushin>
2022-03-27 10:47:53 +0700

    Add new scene

---
+++ MergeTower/Assets/Scripts/Core/Coroutines.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Core
+{
+    public class Coroutines : MonoBehaviour
+    {
+        private const string NAME_OBJECT = "[COROUTINES_OBJECT]";
+
+        private static Coroutines instance => GetInstance();
+        private static Coroutines m_instance;
+
+        private static Coroutines GetInstance()
+        {
+            if (m_instance == null)
+            {
+                CreateCoroutines();
+            }
+
+            return m_instance;
+        }
+
+        private static Coroutines CreateCoroutines()
+        {
+            m_instance = new GameObject(NAME_OBJECT).AddComponent<Coroutines>();
+            m_instance.hideFlags = HideFlags.HideAndDontSave;
+            return m_instance;
+        }
+
+        private static Coroutine StartRoutine(IEnumerator enumerator)
+        {
+            return instance.StartCoroutine(enumerator);
+        }
+    }
+}
